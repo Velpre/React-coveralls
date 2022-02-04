10b2a7bb419322b05b6424b57061f91e39a41830
Velpre <veljko_kv@hotmail.com>
2022-02-04 15:48:40 +0100

    actions

--- index.jsx
+++ index.jsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { ListMovies } from "./movieApplication"
+import { ListMovies } from "./movieApplication";
 
 ReactDOM.render(
-    <ListMovies movies={["My movies"]} />,
-    document.getElementById("app")
+  <ListMovies movies={["My movies"]} />,
+  document.getElementById("app")
 );
